d725af05e6ce968165d53769434d29882a583a1d
jclee8888 <jeffreyclee@gmail.com>
2022-11-07 13:54:41 -0800

    fixed servers, added pictures

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -5,106 +5,92 @@ const controller = {};
 
 
 
-// destination, number of people, budget
+// middleware to get plane ticket price
 controller.getPlaneData = (req, res, next) => {
-    const {
-        destination,
-        passengers,
-        budget,
-        savings,
-        idAPI,
-        planePricePreference} = req.body;
-    res.locals.destination = destination;
-    res.locals.passengers = passengers;
-    res.locals.budget = budget;
-    res.locals.savings = savings;
-    res.locals.idAPI = idAPI;
-    res.locals.planePricePreference = planePricePreference;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'bb4fb94ff1msh97b362efb2e3b6cp11ba3ajsn2104da1b763a',
-            'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
-        }
-    };
-    
-    fetch(`https://skyscanner44.p.rapidapi.com/search?adults=${res.locals.passengers}&origin=MUC&destination=${res.locals.destination}&departureDate=2022-11-11&returnDate=2022-11-18`, options)
-        .then(response => response.json())
-        .then(response => {
-            //console.log('plane response', response)
-            res.locals.displayPlanePrice = response.itineraries.buckets[Number(res.locals.planePricePreference)].items[0].price.raw;
-            console.log(res.locals.displayPlanePrice)
-            res.locals.savings = res.locals.budget-res.locals.displayPlanePrice;
-            console.log("savings:", res.locals.savings)
-            return next();
-        })
-        .catch(err => console.error(err));
-}
-
-
-controller.getHotelData = (req,res,next) => {
-    const hotelPreference = req.body.hotelPreference;
-    res.locals.hotelPreference = hotelPreference;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'bb4fb94ff1msh97b362efb2e3b6cp11ba3ajsn2104da1b763a',
-            'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
-        }
-    };
-
-fetch(`https://skyscanner44.p.rapidapi.com/search-hotel?locationId=${res.locals.idAPI}&adults=${res.locals.passengers}&rooms=${res.locals.passengers}&checkin=2022-11-11&checkout=2022-11-18`, options)
-  .then(response => response.json())
-  .then(response => {
-    res.locals.displayHotelPrice = response.hotels[0].rawPrice;
-
-    console.log("hotel", res.locals.displayHotelPrice)  
-    res.locals.savings-=res.locals.displayHotelPrice;
-    console.log("savings after hotel", res.locals.savings)
-    return next()
-  })
-  .catch(err => console.error(err));
-
-}
-
-controller.getCarData = (req,res,next) => {
-    console.log("do we get here")
-    const carTypePreference = req.body.carTypePreference;
-    res.locals.carTypePreference = carTypePreference;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'bb4fb94ff1msh97b362efb2e3b6cp11ba3ajsn2104da1b763a',
-            'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
-        }
-    };
-
-fetch(`https://skyscanner44.p.rapidapi.com/search-rentacar?pickupId=${res.locals.idAPI}&pickupDate=2022-11-11&pickupTime=10%3A00&returnDate=2022-11-18&returnTime=10%3A00`, options)
+//deconstruct request body to pass to api call
+  const {
+    destination,
+    passengers,
+    budget,
+    savings,
+    idAPI,
+    planePricePreference} = req.body;
+  //populate response object
+  res.locals.destination = destination;
+  res.locals.passengers = passengers;
+  res.locals.budget = budget;
+  res.locals.savings = savings;
+  res.locals.idAPI = idAPI;
+  res.locals.planePricePreference = planePricePreference;
+        
+  //create headers for api call
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'bb4fb94ff1msh97b362efb2e3b6cp11ba3ajsn2104da1b763a',
+      'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
+    }
+  };
+    //api call for plane ticket prices
+  fetch(`https://skyscanner44.p.rapidapi.com/search?adults=${res.locals.passengers}&origin=MUC&destination=${res.locals.destination}&departureDate=2022-11-11&returnDate=2022-11-18`, options)
     .then(response => response.json())
     .then(response => {
-        console.log("carType", res.locals.carTypePreference)
-        for(let i = 0;i<100;i++) {
+      res.locals.displayPlanePrice = response.itineraries.buckets[Number(res.locals.planePricePreference)].items[0].price.raw;
+      res.locals.savings = res.locals.budget-res.locals.displayPlanePrice;
+      return next();
+    })
+    .catch(err => console.error(err));
+};
+
+//middleware to get hotel price
+controller.getHotelData = (req,res,next) => {
+  const hotelPreference = req.body.hotelPreference;
+  res.locals.hotelPreference = hotelPreference;
+  //api call headers
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'bb4fb94ff1msh97b362efb2e3b6cp11ba3ajsn2104da1b763a',
+      'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
+    }
+  };
+  //api call for hotel price
+  fetch(`https://skyscanner44.p.rapidapi.com/search-hotel?locationId=${res.locals.idAPI}&adults=${res.locals.passengers}&rooms=${res.locals.passengers}&checkin=2022-11-11&checkout=2022-11-18`, options)
+    .then(response => response.json())
+    .then(response => {
+      res.locals.displayHotelPrice = response.hotels[0].rawPrice;
+      res.locals.savings -= res.locals.displayHotelPrice;
+      return next();
+    })
+    .catch(err => console.error(err));
+};
+
+//middleware to get car data
+controller.getCarData = (req,res,next) => {
+  const carTypePreference = req.body.carTypePreference;
+  res.locals.carTypePreference = carTypePreference;
+  
+  //api headers
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'bb4fb94ff1msh97b362efb2e3b6cp11ba3ajsn2104da1b763a',
+      'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
+    }
+  };
+
+  //api call for car rental price
+  fetch(`https://skyscanner44.p.rapidapi.com/search-rentacar?pickupId=${res.locals.idAPI}&pickupDate=2022-11-11&pickupTime=10%3A00&returnDate=2022-11-18&returnTime=10%3A00`, options)
+    .then(response => response.json())
+    .then(response => {
+      for(let i = 0; i < 300; i++) {
         if(response.quotes[i].car_name.split(" ")[0]===res.locals.carTypePreference) {
-            res.locals.displayCarPrice = Math.floor(response.quotes[i].price)
-            console.log("carPrice", res.locals.displayCarPrice)
-            res.locals.savings-=res.locals.displayCarPrice;
-            console.log("savings after car", res.locals.savings)
+          res.locals.displayCarPrice = Math.floor(response.quotes[i].price);
+          res.locals.savings-=res.locals.displayCarPrice;
+          return next();
         }
-        return next();
-        }
-        return next();
-        // for(let i = 0;i<response.quotes.length;i++) {
-        //     if(response.quotes[i].car_name.split(" ")[0]===res.locals.carTypePreference) {
-        //         res.locals.displayCarPrice = Math.floor(response.quotes[i].price)
-        //         console.log("carPrice", res.locals.displayCarPrice)
-        //         res.locals.savings-=res.locals.displayCarPrice;
-        //         console.log("savings after car", res.locals.savings)
-        //         return next()
-        //     }
-        // }
-        // return next((err) => {
-        //     log:"car not found"
-        // })
+      }
+      return next();
     })
     .catch(err => console.error(err));
     
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,111 +1,112 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { useState, useEffect} from 'react';
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 import Select from './Select';
 import Axios from "axios";
 
 const Summary = () => {
-    const[displayPlane,setDisplayPlane] = useState()
-    const[displayHotel, setDisplayHotel] = useState()
-    const[displayCar, setDisplayCar] = useState()
-    const[displaySavings, setDisplaySavings] = useState()
-    const location = useLocation();
-    const savings = location.state.budget;
+  const[displayPlane,setDisplayPlane] = useState('');
+  const[displayHotel, setDisplayHotel] = useState('');
+  const[displayCar, setDisplayCar] = useState('');
+  const[displaySavings, setDisplaySavings] = useState('');
+  const location = useLocation();
+  const savings = location.state.budget;
 
-    console.log('destination: ', location.state.destination)
-    console.log('budget: ',location.state.budget)
-    console.log('location: ',location.state.passengers)
-    console.log('idAPI: ',location.state.idAPI)
-    console.log('planePreference: ',location.state.planePricePreference)
-    console.log('hotelPreference: ',location.state.hotelPreference)
-    console.log('cartype: ',location.state.carTypePreference)
-    console.log('savings',savings)
+//   console.log('destination: ', location.state.destination);
+//   console.log('budget: ',location.state.budget);
+//   console.log('location: ',location.state.passengers);
+//   console.log('idAPI: ',location.state.idAPI);
+//   console.log('planePreference: ',location.state.planePricePreference);
+//   console.log('hotelPreference: ',location.state.hotelPreference);
+//   console.log('cartype: ',location.state.carTypePreference);
+//   console.log('savings',savings);
 
-    
-
-    //if this doesn't work we do onClick on a button that says CALCULATE!
-    // useEffect(() => {
-    //     Axios.post('http://localhost:1234/api/', {
-    //         destination: location.state.destination,
-    //         passengers: location.state.passengers,
-    //         budget:location.state.budget,
-    //         savings: savings,
-    //         idAPI: location.state.idAPI,
-    //         planePricePreference: location.state.planePricePreference,
-    //         hotelPreference: location.state.hotelPreference,
-    //         carTypePreference:location.state.carType,
-    //     })
-    //     // setDisplayPlane(res.locals.displayPlanePrice)
-    // })
-    
-    
-    
-    // useEffect(() => {
-    //     fetch('/api', {
-    //         method: 'POST',
-    //         headers: {
-    //           'Content-Type': 'Application/JSON',
-    //         },
-    //         body: JSON.stringify({
-    //             destination: location.state.destination,
-    //             passengers: location.state.passengers,
-    //             budget:location.state.budget,
-    //             savings: savings,
-    //             idAPI: location.state.idAPI,
-    //             planePricePreference: location.state.planePricePreference,
-    //             hotelPreference: location.state.hotelPreference,
-    //             carTypePreference:location.state.carType,
-    //         }),
-    //       })
-    //         .then((res) => {res.json()})
-    //         .then((res) => {
-    //             setDisplayPlane(res.locals.displayPlanePrice);
-    //             setDisplayHotel(res.locals.displayHotelPrice);
-    //             setDisplayCar(res.locals.displayCarPrice);
-    //             setDisplaySavings(res.locals.savings);
-    //         })
-    //         .catch((error) => console.log('ERROR: could not post-fetch: ' + error));
-      
-    //   }, []);
-    const handleCalculate= (e) => {
-        console.log("carType handleCalculate", location.state.carTypePreference)
-        fetch('/api', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'Application/JSON',
-                    },
-                    body: JSON.stringify({
-                        destination: location.state.destination,
-                        passengers: location.state.passengers,
-                        budget:location.state.budget,
-                        savings: savings,
-                        idAPI: location.state.idAPI,
-                        planePricePreference: location.state.planePricePreference,
-                        hotelPreference: location.state.hotelPreference,
-                        carTypePreference:location.state.carTypePreference,
-                    }),
-                  })
-                    .then((res) => {res.json()})
-                    .then((res) => { console.log({res})
-                        setDisplayPlane(res.displayPlanePrice);
-                        setDisplayHotel(res.displayHotelPrice);
-                        setDisplayCar(res.displayCarPrice);
-                        setDisplaySavings(res.savings);
-                    })
-                    .catch((error) => console.log('ERROR: could not post-fetch: ' + error));
-    }
+  const handleCalculate= (e) => {
+    console.log("carType handleCalculate", location.state.carTypePreference);
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON',
+      },
+      body: JSON.stringify({
+        destination: location.state.destination,
+        passengers: location.state.passengers,
+        budget:location.state.budget,
+        savings: savings,
+        idAPI: location.state.idAPI,
+        planePricePreference: location.state.planePricePreference,
+        hotelPreference: location.state.hotelPreference,
+        carTypePreference:location.state.carTypePreference,
+      }),
+    })
+    .then(response => response.json())
+    .then((response) => {
+        //   console.log("res:", response);
+          setDisplayPlane(response.displayPlanePrice);
+          setDisplayHotel(response.displayHotelPrice);
+          setDisplayCar(response.displayCarPrice);
+          setDisplaySavings(response.savings);
+        //   console.log("plane display", displayPlane);
+        })
+                        
+    .catch((error) => console.log('ERROR: could not post-fetch: ' + error));
+  };
 
     
-    return (
-        <div className = "displayPage">
-            <button onClick = {handleCalculate} className = "calculate">CALCULATE</button>
-            <div className = "displayPlane">{displayPlane}</div>
-            <div className = "displayHotel">{displayHotel}</div>
-            <div className = "displayCar">{displayCar}</div>
-            <div className = "displaySavings">{displaySavings}</div>
-        </div>
-    )
-}
+  return (
+    <>
+    <div id = "display-table">
+    <button onClick = {handleCalculate} className = "calculate">CALCULATE</button>
+    
+    <div className = "table-display">
+       <tr>
+            <th> Plane Cost </th>
+            <th> Hotel Cost </th>
+            <th> Car Cost </th>
+            <th> Savings </th>
+        </tr>
+        <tr> 
+            <th> {displayPlane} </th>
+            <th> {displayHotel} </th>
+            <th>  {displayCar} </th>
+            <th> {displaySavings}</th>
+        </tr>
+
+    </div>
+    </div>
+    </>
+  );
+};
 
 export default Summary;  
+
+
+{/* <tbody>
+<tr>
+    <th>User Id</th>
+    <th>Id</th>
+    <th>Title</th>
+    <th>Description</th>
+</tr>
+{data.map((item, i) => (
+    <tr key={i}>
+        <td>{item.userId}</td>
+        <td>{item.id}</td>
+        <td>{item.title}</td>
+        <td>{item.body}</td>
+    </tr>
+))}
+</tbody> */}
+
+/* <div className = "displayPage">
+      <button onClick = {handleCalculate} className = "calculate">CALCULATE</button>
+      <div className = "displayPlane">Plane Cost: {displayPlane}</div>
+      <div className = "displayHotel"> Hotel Cost: {displayHotel}</div>
+      <div className = "displayCar"> Car Cost: {displayCar}</div>
+      <div className = "displaySavings"> Savings: {displaySavings}</div>
+    </div> */
+
+    //   console.log("hotel display", displayHotel);
+        //   console.log("car display", displayCar);
+        //   console.log("save display", displaySavings);
